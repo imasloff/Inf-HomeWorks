105eb8a998821661f03517500a7285211a1acec2
imasloff <74798659+imasloff@users.noreply.github.com>
2020-11-30 21:44:46 +0300

    Update class_triangle.py

--- class_triangle.py
+++ class_triangle.py
@@ -2,14 +2,14 @@ class MyClass:
     def __init__(self, x):
         self.x = int(x)
 
-    def check(self):
-        self.a = int(input("enter the first angle: "))
-        self.b = int(input("enter the second angle: "))
-        self.c = int(input("enter the third angle: "))
-        if ((self.a + self.b + self.c) == self.x) and (self.a > 0) and (self.b > 0) and (self.c > 0):
+    def check(self, a, b, c):
+        if (self.x == a + b + c) and (a > 0) and (b > 0) and (c > 0):
             print("these angles could belong to a triangle!")
         else :
             print("these angles couldn't belong to a triangle!")
 
 tr = MyClass(180)
-tr.check()
+a = int(input("enter the first angle: "))
+b = int(input("enter the second angle: "))
+c = int(input("enter the third angle: "))
+tr.check(a, b, c)
