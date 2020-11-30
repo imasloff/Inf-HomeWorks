cfba130a55f6cc610be5993ac94a037036184501
imasloff <74798659+imasloff@users.noreply.github.com>
2020-11-30 21:47:45 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -6,3 +6,4 @@ if ((a + b + c) == 180) and (a > 0) and (b > 0) and (c > 0):
     print("these angles could belong to a triangle!")
 else:
     print("these angles couldn't belong to a triangle!")
+    
