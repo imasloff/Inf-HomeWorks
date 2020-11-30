03e3e9d7bacd846594b6f9466c5ba0b19c604609
imasloff <74798659+imasloff@users.noreply.github.com>
2020-11-30 21:47:06 +0300

    Update class_x.py

--- class_x.py
+++ class_x.py
@@ -2,14 +2,14 @@ class MyClass:
     def __init__(self, x):
         self.x = int(x)
 
-    def check(self):
-        self.a = int(input("enter the first number: "))
-        self.b = int(input("enter the second number: "))
-        self.c = int(input("enter the third number: "))
-        if (self.a + self.b + self.c) == self.x :
+    def check(self, a, b, c):
+        if a + b + c == self.x :
             print("sum = " + str(self.x))
         else :
             print("sum != " + str(self.x))
 
 ob1 = MyClass(90)
-ob1.check()
+a = int(input("enter the first number: "))
+b = int(input("enter the second number: "))
+c = int(input("enter the third number: "))
+ob1.check(a, b, c)
