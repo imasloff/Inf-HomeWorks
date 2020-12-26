3e481d9a0ad1256cd515636a43c4f4bab082cbcd
imasloff <74798659+imasloff@users.noreply.github.com>
2020-12-26 18:24:59 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,42 +4,70 @@ from stack import Stack
 
 class TestStack(unittest.TestCase):
     def setUp(self):
-        print("setUp")
         self.st1 = Stack()
-        self.st2 = Stack()
-        self.st3 = Stack()
-        self.st1.push(1)
-        self.st1.push(2)
-        self.st1.push(3)
-        self.st2.push(True)
-        self.st2.push(False)
-
-    def test_size(self):
-        print("test_size")
-        self.assertIs(self.st1.get_size(), 3)
-        self.assertIs(self.st2.get_size(), 2)
-        self.assertIs(self.st3.get_size(), 0)
 
     def tearDown(self):
-        print("tearDown")
-        self.st1.clear()
-        self.st2.clear()
-        self.st3.clear()
         del(self.st1)
-        del(self.st2)
-        del(self.st3)
+
+    def test_push(self):
+        self.st1.push(1)
+        self.assertIs(self.st1.cur_top.data, 1)
+        self.assertIs(self.st1.cur_top.prev, None)
+        self.assertIs(self.st1.size, 1)
+        self.st1.push(True)
+        self.assertIs(self.st1.cur_top.data, True)
+        self.assertIs(self.st1.size, 2)
+        self.st1.push('IVAN')
+        self.assertIs(self.st1.cur_top.data, 'IVAN')
+        self.assertIs(self.st1.size, 3)
 
     def test_pop(self):
-        print("test_pop")
-        self.assertIs(self.st1.pop(), 3)
-        self.assertIs(self.st2.pop(), False)
-        self.assertIs(self.st3.pop(), None)
+        self.st1.push(1)
+        self.st1.push(True)
+        self.st1.push('IVAN')
+        self.assertIs(self.st1.cur_top.data, 'IVAN')
+        self.assertIs(self.st1.pop(), 'IVAN')
+        self.assertIs(self.st1.cur_top.data, True)
+        self.assertIs(self.st1.pop(), True)
+        self.assertIs(self.st1.cur_top.data, 1)
+        self.assertIs(self.st1.pop(), 1)
+        self.assertIs(self.st1.cur_top, None)
+        self.assertIs(self.st1.pop(), None)
+        self.assertIs(self.st1.cur_top, None)
 
     def test_top(self):
-        print("test_top")
-        self.assertIs(self.st1.top(), 3)
-        self.assertIs(self.st2.top(), False)
-        self.assertIs(self.st3.top(), None)
+        self.st1.push(1)
+        self.st1.push(True)
+        self.st1.push('IVAN')
+        self.assertIs(self.st1.top(), 'IVAN')
+        self.st1.pop()
+        self.assertIs(self.st1.top(), True)
+        self.st1.pop()
+        self.assertIs(self.st1.top(), 1)
+        self.st1.pop()
+        self.assertIs(self.st1.top(), None)
+
+    def test_size(self):
+        self.st1.push(1)
+        self.st1.push(True)
+        self.st1.push('IVAN')
+        self.assertIs(self.st1.get_size(), 3)
+        self.st1.pop()
+        self.assertIs(self.st1.get_size(), 2)
+        self.st1.pop()
+        self.assertIs(self.st1.get_size(), 1)
+        self.st1.pop()
+        self.assertIs(self.st1.get_size(), 0)
+        self.st1.pop()
+        self.assertIs(self.st1.get_size(), 0)
+
+    def test_clear(self):
+        self.st1.push(1)
+        self.st1.push(True)
+        self.st1.push('IVAN')
+        self.st1.clear()
+        self.assertIs(self.st1.size, 0)
+        self.assertIs(self.st1.cur_top, None)
 
 if __name__ == '__main__':
     unittest.main()
